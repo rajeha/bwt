51a124781b81138dd74a1f24205a215b667143fb
Ahmad Rajeh <ahmadrjeh@gmail.com>
2016-10-03 20:51:30 -0500

    Update bwt_create.pl

--- bwt_create.pl
+++ bwt_create.pl
@@ -7,7 +7,7 @@ sub bwt {
 	my @chars = split //, $str;
 
 	my @suff = sort {"@chars[$a..$#chars] @chars[0..$a-1]" cmp "@chars[$b..$#chars] @chars[0..$b-1]"} 0..$#chars;
-	join "", map {$_--?"$chars[$_]":"[$chars[$_]]"} @suff;
+	join "", map {$_--? "$chars[$_]": "[$chars[$_]]"} @suff;
 }
 
 while (<>) {
